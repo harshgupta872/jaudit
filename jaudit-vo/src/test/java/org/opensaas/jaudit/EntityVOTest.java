79719c0b13414d6a34550c4ed5572da386276f82
george.qiao <george.qiao@6e62a153-e647-0410-9589-e939b650f62c>
2008-04-01 03:44:13 +0000

    task: add unit test reviewer: Matt

---
+++ jaudit-vo/src/test/java/org/opensaas/jaudit/EntityVOTest.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE
+ * You may obtain a copy of the License at
+ *
+ *   http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opensaas.jaudit;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link EntityVO}.
+ */
+public class EntityVOTest {
+
+    @Test
+    public void testConstructor() {
+        EntityVO eVO = new EntityVO();
+        assert eVO != null;
+        assert eVO instanceof AuditSubject;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullId() {
+        EntityVO eVO = new EntityVO();
+        assert eVO.getId() == null;
+        eVO.setId(null);
+    }
+
+    @Test
+    public void testEmptyId() {
+        EntityVO eVO = new EntityVO();
+        eVO.setId("");
+        assert eVO.getId().equals("");
+    }
+
+    @Test
+    public void testId() {
+        EntityVO eVO = new EntityVO();
+        eVO.setId("a");
+        assert eVO.getId().equals("a");
+    }
+
+    @Test
+    public void testSubjectType() {
+        EntityVO eVO = new EntityVO();
+        eVO.setSubjectType("a");
+        assert eVO.getSubjectType().equals("a");
+    }
+}
