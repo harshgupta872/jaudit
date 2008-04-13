0c33c58c500df4058080a415e420fc193751c5dc
mwkinman <mwkinman@6e62a153-e647-0410-9589-e939b650f62c>
2008-04-13 23:18:00 +0000

    Task: New audit service interface and fixes to current properties.

--- trunk/jaudit-dao-hibernate/src/main/java/org/opensaas/jaudit/dao/hibernate/GenericDaoHibernate.java
+++ trunk/jaudit-dao-hibernate/src/main/java/org/opensaas/jaudit/dao/hibernate/GenericDaoHibernate.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.opensaas.jaudit.hibernate;
+package org.opensaas.jaudit.dao.hibernate;
 
 import java.io.Serializable;
 
