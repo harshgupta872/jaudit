21288476019044529b5a1bb59754a931cfaf37ec
mwkinman <mwkinman@6e62a153-e647-0410-9589-e939b650f62c>
2008-04-13 23:17:32 +0000

    Task: New audit service interface and fixes to current properties.

---
+++ jaudit-service-default/src/main/java/org/opensaas/jaudit/service/impl/AuditServiceImpl.java
@@ -0,0 +1,177 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE
+ * You may obtain a copy of the License at
+ *
+ *   http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opensaas.jaudit.service.impl;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.opensaas.jaudit.AuditSubject;
+import org.opensaas.jaudit.ResponsibleInformation;
+import org.opensaas.jaudit.SessionRecord;
+import org.opensaas.jaudit.SessionRecordMutable;
+import org.opensaas.jaudit.dao.SessionRecordDao;
+import org.opensaas.jaudit.service.AuditService;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ * The default implementation of {@link AuditService}.
+ */
+public class AuditServiceImpl implements AuditService {
+
+    static private final Logger LOGGER = Logger
+            .getLogger(AuditServiceImpl.class.getName());
+
+    /**
+     * For interacting with the persistence layer.
+     */
+    private SessionRecordDao _sessionRecordDao;
+
+    /**
+     * Will return new {@link AuditSubject} objects.
+     */
+    private ObjectFactory _auditSubjectFactory;
+
+    /**
+     * Will return new {@link ResponsibleInformation} objects.
+     */
+    private ObjectFactory _responsibleInformationFactory;
+
+    /**
+     * Will return new {@link SessionRecord} objects.
+     */
+    private ObjectFactory _sessionRecordFactory;
+
+    /**
+     * Will be used to fill in new session records.
+     */
+    private AuditSubject _auditSystem;
+
+    /**
+     * Sets the audit system address that will be filled in on new session
+     * records.
+     */
+    private String _auditSystemAddress;
+
+    /**
+     * {@inheritDoc}
+     */
+    public AuditSubject newAuditSubjectMutable() {
+        return (AuditSubject) _auditSubjectFactory.getObject();
+    }
+
+    public ResponsibleInformation newResponsibleInformation() {
+        return (ResponsibleInformation) _responsibleInformationFactory
+                .getObject();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public SessionRecord createSessionRecord(String sessionId,
+            ResponsibleInformation responsibleInformation) {
+        SessionRecordMutable sessionRecord = (SessionRecordMutable) _sessionRecordFactory
+                .getObject();
+
+        sessionRecord.setSessionId(sessionId);
+        sessionRecord.setResponsibleInformation(responsibleInformation);
+        sessionRecord.setStartedTs(new Date());
+        sessionRecord.setSystem(_auditSystem);
+        sessionRecord.setSystemAddress(_auditSystemAddress);
+
+        String id = _sessionRecordDao.create(sessionRecord);
+        LOGGER.log(Level.FINE, "Created new session record with id {0}.", id);
+        return sessionRecord;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public SessionRecord createSessionRecord() {
+        return createSessionRecord(null, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void sessionEnded(SessionRecord sessionRecord) {
+        SessionRecordMutable srm = _sessionRecordDao
+                .read(sessionRecord.getId());
+        _sessionRecordDao.updateEndedTs(srm, new Date());
+    }
+
+    /**
+     * Sets the required responsible information factory.
+     * 
+     * @param responsibleInformationFactory
+     */
+    @Required
+    public void setResponsibleInformationFactory(
+            ObjectFactory responsibleInformationFactory) {
+        _responsibleInformationFactory = responsibleInformationFactory;
+    }
+
+    /**
+     * Sets the required session record dao which allows us to complete our
+     * work.
+     * 
+     * @param sessionRecordDao
+     *            SessionRecordDao to set.
+     */
+    @Required
+    public void setSessionRecordDao(SessionRecordDao sessionRecordDao) {
+        _sessionRecordDao = sessionRecordDao;
+    }
+
+    /**
+     * Sets the required sessionRecordFactory which will give us new
+     * SessionRecord objects.
+     * 
+     * @param sessionRecordFactory
+     *            SessionRecordFactory to set.
+     */
+    @Required
+    public void setSessionRecordFactory(ObjectFactory sessionRecordFactory) {
+        _sessionRecordFactory = sessionRecordFactory;
+    }
+
+    /**
+     * Sets the optional audit system.
+     * 
+     * @param auditSystem
+     */
+    public void setAuditSystem(AuditSubject auditSystem) {
+        _auditSystem = auditSystem;
+    }
+
+    /**
+     * Sets the optional audit system address.
+     * 
+     * @param auditySystemAddress
+     */
+    public void setAuditSystemAddress(String auditSystemAddress) {
+        _auditSystemAddress = auditSystemAddress;
+    }
+
+    /**
+     * Sets the required audit subject factory.
+     * 
+     * @param auditSubjectFactory
+     */
+    @Required
+    public void setAuditSubjectFactory(ObjectFactory auditSubjectFactory) {
+        _auditSubjectFactory = auditSubjectFactory;
+    }
+
+}
