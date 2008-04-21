884c3a261c6ed47604397cb573e3db34226172dc
mwkinman <mwkinman@6e62a153-e647-0410-9589-e939b650f62c>
2008-04-21 13:14:22 +0000

    Task: Add fake FooBar domain and initial annotations for auditing.

---
+++ trunk/jaudit/src/main/java/org/opensaas/jaudit/LifeCycleAudit.java
@@ -0,0 +1,26 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE
+ * You may obtain a copy of the License at
+ *
+ *   http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opensaas.jaudit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LifeCycleAudit {
+
+    LifeCycleType type();
+
+}
